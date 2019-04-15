4631b57dc8cc7dfa98b7937f073bb7376e362478
mswdwk <mswdwk@outlook.com>
2019-04-16 00:37:56 +0800

    feat: parse pb_data with less filed

--- codes/pb_test/src/main.rs
+++ codes/pb_test/src/main.rs
@@ -34,11 +34,31 @@ fn test_parse_pbdata_with_more_fields() -> Result<(), ProtobufError> {
 
     Ok(())
 }
+
+fn test_parse_pbdata_with_less_fields() -> Result<(), ProtobufError> {
+    let mut content_str4 = ContentStr::new();
+    content_str4.set_one(true);
+    content_str4.set_two("1".into());
+    content_str4.set_three("hello".to_string());
+
+    let bytes = content_str4.write_to_bytes()?;
+    let cont_str = parse_from_bytes::<ContentStr4>(&bytes)?;
+    println!("one= {:?}", cont_str.get_one());
+    println!("two= {:?}", cont_str.get_two());
+    println!("three= {:?}", cont_str.get_three());
+    println!("four= {:?}", cont_str.get_four());
+
+    Ok(())
+}
+
 fn main() {
     if let Err(err) = test_parse_pbdata_with_field_type_different() {
         println!("err= {:?}", err);
     }
     if let Err(err) = test_parse_pbdata_with_more_fields() {
+        println!("err= {:?}", err);
+    }
+    if let Err(err) = test_parse_pbdata_with_less_fields() {
         println!("err= {:?}", err);
     }
     self::fut::f_main();
