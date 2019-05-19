6063f225c65c849c4fa96f9d115dbf00f54a414a
mswdwk <mswdwk@outlook.com>
2019-05-19 13:31:02 +0800

    feat: add a trait test

---
+++ codes/trait_test/src/main.rs
@@ -0,0 +1,25 @@
+// fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+// https://doc.rust-lang.org/book/ch10-02-traits.html#fixing-the-largest-function-with-trait-bounds
+fn largest<T: PartialOrd >(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for ref item in list.iter() {
+        if *item > &*largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
