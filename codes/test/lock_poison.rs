e06eab084ca258548f97d60415dcd119c1a0e5e3
mswdwk <mswdwk@outlook.com>
2019-06-19 11:11:24 +0800

    test: caculate cost time of read once instant

--- codes/test/lock_poison.rs
+++ codes/test/lock_poison.rs
@@ -28,5 +28,23 @@ fn main() {
     }
     let escp  = now.elapsed().as_nanos();
     let per = escp / loop_time;
-    println!("total cost= {}ms once_cost= {}ns",now.elapsed().as_millis(),per  );
+    println!("rwlock total cost= {}ms once_cost= {}ns",now.elapsed().as_millis(),per  );
+    
+    let now = Instant::now();
+    let loop_time = 10_000_000;
+    for i in  0..loop_time {
+       let _ = i;
+    }
+    let escp  = now.elapsed().as_nanos();
+    let per = escp / loop_time;
+    println!("read mem total cost= {}ms once_cost= {}ns",now.elapsed().as_millis(),per  );
+    
+    let now = Instant::now();
+    for _ in  0..loop_time {
+       let _ = now.elapsed().as_secs();
+    }
+    let escp  = now.elapsed().as_nanos();
+    let per = escp / loop_time;
+    println!("read Instant total cost= {}ms once_cost= {}ns",now.elapsed().as_millis(),per  );
+
 }
